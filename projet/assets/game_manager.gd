a0f7e61ea6b05b89e1620b15ad8488ff68d4b401
MatMatHelou <143528499+MatMatHelou@users.noreply.github.com>
2025-12-12 13:20:53 -0500

    mainmenu

--- projet/assets/game_manager.gd
+++ projet/assets/game_manager.gd
@@ -1,3 +1,12 @@
 extends Node
 
+@onready var points_label: Label = $"../UI/Panel/PointsLabel"
+@onready var game_manager: Node = %GameManager
+
 var points = 0
+
+func add_point():
+	points += 1
+	print(points)
+	game_manager.add_points()
+	points_label.text = "Points: " + str(points)
